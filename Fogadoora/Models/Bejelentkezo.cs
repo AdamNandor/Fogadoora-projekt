4c99f37a2f6ccc784c06951409852a8c665f581d
AdamNandor <adamn@kkszki.hu>
2026-02-19 13:15:33 +0100

    Bejelentkezo,fogadoora osztály

--- Fogadoora/Models/Bejelentkezo.cs
+++ Fogadoora/Models/Bejelentkezo.cs
@@ -8,5 +8,24 @@ namespace Fogadoora.Models
 {
     internal class Bejelentkezo
     {
+        public Bejelentkezo(int id, string nev, string email, string tel)
+        {
+            Id = id;
+            Nev = nev;
+            Email = email;
+            Tel = tel;
+        }
+
+        public Bejelentkezo() { }
+
+        public int Id { get; set; }
+        public string Nev { get; set; }
+        public string Email { get; set; }
+        public string Tel { get; set; }
+
+        public override string ToString()
+        {
+            return $"Id: {Id}\nNév: {Nev}\nEmail: {Email}\nTelefonszám: {Tel}";
+        }
     }
 }
--- Fogadoora/Models/Fogadoora.cs
+++ Fogadoora/Models/Fogadoora.cs
@@ -8,5 +8,24 @@ namespace Fogadoora.Models
 {
     internal class Fogadoora
     {
+        public Fogadoora(int id, string hely, DateTime kezdet, int hossz)
+        {
+            Id = id;
+            Hely = hely;
+            Kezdet = kezdet;
+            Hossz = hossz;
+        }
+
+        public Fogadoora() { }
+
+        public int Id { get; set; }
+        public string Hely { get; set; }
+        public DateTime Kezdet { get; set; }
+        public int Hossz { get; set; }
+
+        public override string ToString()
+        {
+            return $"Id: {Id}\nHely: {Hely}\nKezdet: {Kezdet}\nHossz: {Hossz}";
+        }
     }
 }
