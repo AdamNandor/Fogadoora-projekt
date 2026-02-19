b393a530f492ce66cbfd47f9ceb49926e6f7fe3b
AdamNandor <adamn@kkszki.hu>
2026-02-19 13:18:19 +0100

    Egyenlőre kész adatbázis

--- Fogadoora/Database/fogadoora.sql
+++ Fogadoora/Database/fogadoora.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Gép: 127.0.0.1
--- Létrehozás ideje: 2026. Feb 19. 13:02
+-- Létrehozás ideje: 2026. Feb 19. 13:17
 -- Kiszolgáló verziója: 10.4.32-MariaDB
 -- PHP verzió: 8.2.12
 
@@ -28,6 +28,7 @@ SET time_zone = "+00:00";
 --
 
 CREATE TABLE `bejelentkezo` (
+  `Id` int(11) NOT NULL,
   `Nev` varchar(32) NOT NULL,
   `Email` varchar(32) NOT NULL,
   `Tel` varchar(32) NOT NULL
@@ -40,10 +41,43 @@ CREATE TABLE `bejelentkezo` (
 --
 
 CREATE TABLE `fogadoora` (
+  `Id` int(11) NOT NULL,
   `Hely` varchar(32) NOT NULL,
   `Kezdet` datetime(6) NOT NULL,
-  `Hossz` time(6) NOT NULL
+  `Hossz` int(6) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Indexek a kiírt táblákhoz
+--
+
+--
+-- A tábla indexei `bejelentkezo`
+--
+ALTER TABLE `bejelentkezo`
+  ADD PRIMARY KEY (`Id`);
+
+--
+-- A tábla indexei `fogadoora`
+--
+ALTER TABLE `fogadoora`
+  ADD PRIMARY KEY (`Id`);
+
+--
+-- A kiírt táblák AUTO_INCREMENT értéke
+--
+
+--
+-- AUTO_INCREMENT a táblához `bejelentkezo`
+--
+ALTER TABLE `bejelentkezo`
+  MODIFY `Id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT a táblához `fogadoora`
+--
+ALTER TABLE `fogadoora`
+  MODIFY `Id` int(11) NOT NULL AUTO_INCREMENT;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
