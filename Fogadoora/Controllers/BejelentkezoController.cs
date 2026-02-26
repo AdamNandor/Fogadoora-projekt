2d3709d74177744ece32936656b1e4e9eaaa5d7d
Szabi <nagysz@kkszki.hu>
2026-02-26 10:26:25 +0100

    MySql package és Bejelentkző lista lekérés

--- Fogadoora/Controllers/BejelentkezoController.cs
+++ Fogadoora/Controllers/BejelentkezoController.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using Fogadoora.Models;
+using MySql.Data;
+using MySql.Data.MySqlClient;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +11,30 @@ namespace Fogadoora.Controllers
 {
     internal class BejelentkezoController
     {
+        public List<Bejelentkezo> GetBejList()
+        {
+            MySqlConnection conn = new MySqlConnection("server=localhost;user=root;password=;database=fogadoora;");
+            conn.Open();
+            string comd = "SELECT * FROM bejelentkezo;";
+            MySqlCommand cmd = new MySqlCommand(comd, conn);
+            List<Bejelentkezo> connections = new List<Bejelentkezo>();
+            using (MySqlDataReader reader = cmd.ExecuteReader())
+            {
+                while (reader.Read())
+                {
+                    connections.Add(new Bejelentkezo(
+                        reader.GetInt32("id"),
+                        reader.GetString("nev"),
+                        reader.GetString("email"),
+                        reader.GetString("tel")
+                    ));
+                }
+
+                conn.Close();
+                return connections;
+            }
+        }
+
+
     }
 }
